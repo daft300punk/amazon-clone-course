199e8dab4f6d6a11836588afc774ab31bc974766
Abhilash Singh <daft300punk@gmail.com>
2017-05-10 01:38:10 +0530

    Add post route

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ UserSchema.pre('save', function(next) {
   if(!user.isModified('password')) return next();
   bcrypt.genSalt(10, (err, salt) => {
     if(err) return next(err);
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
       if(err) return next(err);
       user.password = hash;
       next();
