38fbfc0df2849dcf46b235fe6b7ed7a22d4423df
Abhilash Singh <daft300punk@gmail.com>
2017-05-09 20:43:07 +0530

    Login

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json("YOYO");
+})
 
 app.listen(3000, (err) => {
   if(err) throw err;
