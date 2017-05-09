e102060d77f3d82440304b323f000e2f99fdef1e
Abhilash Singh <daft300punk@gmail.com>
2017-05-09 22:42:36 +0530

    User model and db configs

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+var dbConfig = require('./db.config');
+var dbUrl = `mongodb://${dbConfig.DB_USERNAME}:${dbConfig.DB_PASSWORD}@ds135577.mlab.com:35577/daft300punk-ecomm`
+mongoose.connect(dbUrl, (err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Connected to db');
+  }
+});
 
 app.use(morgan('dev'));
 
