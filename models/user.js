9e527fb6de66319b995834c390d36f556f07658d
Abhilash Singh <daft300punk@gmail.com>
2017-05-10 01:38:10 +0530

    Add post route

--- models/user.js
+++ models/user.js
@@ -19,10 +19,13 @@ var UserSchema = new Schema({
 
 UserSchema.pre('save', function(next) {
   var user = this;
+  console.log('pre');
   if(!user.isModified('password')) return next();
   bcrypt.genSalt(10, (err, salt) => {
+    console.log('inside gensalt', err, salt);
     if(err) return next(err);
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      console.log('inside hash', err, hash);
       if(err) return next(err);
       user.password = hash;
       next();
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
+
+mongoose.Promise = Promise;
 
 var dbConfig = require('./db.config');
 var dbUrl = `mongodb://${dbConfig.DB_USERNAME}:${dbConfig.DB_PASSWORD}@ds135577.mlab.com:35577/daft300punk-ecomm`
@@ -15,10 +20,23 @@ mongoose.connect(dbUrl, (err) => {
 });
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.json("YOYO");
-})
+app.post('/create-user', (req, res, next) => {
+  console.log('inside post route');
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+  console.log(req.body);
+  user.save((err) => {
+    if(err) return next(err);
+
+    res.json('Successfully created a user');
+  });
+});
 
 app.listen(3000, (err) => {
   if(err) throw err;
